a191a64a391cb111375e99e65d54fc2b5c8e3099
unknown <mohamedmmedhat21@gmail.com>
2022-11-06 15:37:18 +0200

    refactoring messages controller

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,67 +1,64 @@
 module Api
     module V1
       class MessagesController < ApplicationController
-  
+        before_action :get_app_chat
+
         def index
-          application = Application.where(token: params[:application_token]).first
-          chat = application.chats.where(number: params[:chat_number]).first
-          messages = chat.messages
+          messages = @chat.messages
           render json: messages.select(:number,:content).to_json(except: :id)
         end
         
         def create
-            application = Application.where(token: params[:application_token]).first
-            chat = application.chats.where(number: params[:chat_number]).first
-            if chat.messages.last.nil?
-              msg_num = 1
-            else
-              msg_num = chat.messages.last.number + 1
-            end
-            message = Message.new(chat_id:chat.id,number:msg_num,content:params[:content])
-    
-            if message.valid?
-              MessageCreationJob.perform_async(message.chat_id,message.number,message.content)
-              render json:{
-                number:message.number
-              }, status: :created
-            else
-              render json:message.errors,status: :unprocessable_entity
-            end
+          if @chat.messages.last.nil?
+            msg_num = 1
+          else
+            msg_num = @chat.messages.last.number + 1
+          end
+          message = Message.new(chat_id:@chat.id,number:msg_num,content:params[:content])
+  
+          if message.valid?
+            MessageCreationJob.perform_async(message.chat_id,message.number,message.content)
+            render json:{
+              number:message.number
+            }, status: :created
+          else
+            render json:message.errors,status: :unprocessable_entity
+          end
         end
 
         def show
-            application = Application.where(token: params[:application_token]).first
-            chat = application.chats.where(number: params[:chat_number]).first
-            message = chat.messages.where(number: params[:number]).first
-            if message.nil?
-              render json: { error: "Couldn't find Message with 'number'= #{params[:number]}" }, status: 404
-            else
-              render json:{
-                  number:message.number,
-                  content:message.content
-              },status: :ok
-            end
+          message = @chat.messages.where(number: params[:number]).first
+          if message.nil?
+            render json: { error: "Couldn't find Message with 'number'= #{params[:number]}" }, status: 404
+          else
+            render json:{
+                number:message.number,
+                content:message.content
+            },status: :ok
+          end
         end
 
 
         def update
-            application = Application.where(token: params[:application_token]).first
-            chat = application.chats.where(number: params[:chat_number]).first
-            message = chat.messages.where(number: params[:number]).first
-            if message.update(content: params[:content])
-                head :no_content
-            else
-                render json: message.errors, status: :unprocessable_entity
-            end
+          message = @chat.messages.where(number: params[:number]).first
+          if message.update(content: params[:content])
+              head :no_content
+          else
+              render json: message.errors, status: :unprocessable_entity
+          end
         end
         
         def search
-          application = Application.where(token: params[:application_token]).first
-          chat = application.chats.where(number: params[:chat_number]).first
-          messages = chat.messages.search_msgs(params[:query],chat.id).records
+          messages = @chat.messages.search_msgs(params[:query],@chat.id).records
           render json: messages.to_json(except: [:id,:chat_id,:created_at,:updated_at])
         end
 
+        private
+
+        def get_app_chat
+          @application = Application.where(token: params[:application_token]).first
+          @chat = @application.chats.where(number: params[:chat_number]).first
+        end
       end
     end
   end
